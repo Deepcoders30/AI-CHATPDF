f64b0ebfbc9f7df6dd2b9eeebe66f0948bcc898f
Deepcoders30 <umair.khan.rocko@gmail.com>
2025-07-05 22:03:09 +0530

    chore: small fix

--- ChatPDF-Client/ChatPDF/src/components/Navbar.tsx
+++ ChatPDF-Client/ChatPDF/src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = ({ getDocId }: NavbarProps) => {
       const formData = new FormData();
       formData.append("pdfFile", file);
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/upload`, {
+      const response = await fetch(`${process.env.SERVER_URL}/upload`, {
         method: "POST",
         body: formData,
       });
